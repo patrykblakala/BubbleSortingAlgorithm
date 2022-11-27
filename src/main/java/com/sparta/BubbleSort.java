f003d6723886a7617f8845d22bef09c97dfb9578
patrykblakala <patrykblakala108@gmail.com>
2022-11-27 11:02:01 +0000

    checking if the input length is less than 2

--- src/main/java/com/sparta/BubbleSort.java
+++ src/main/java/com/sparta/BubbleSort.java
@@ -3,6 +3,9 @@ package com.sparta;
 public class BubbleSort {
 
     public static int[] bubbleSort(int[] input) {
+        if (input.length <=1) {
+            return input;
+        }
         for (int i = 0; i < input.length - 1; i++) {
             for (int j = 0; j < input.length - 1; j++) {
                 if (input[j] > input[j + 1]) {
